29d6a5709ddb79a979349d472241475a82a89df5
harammal <hassanrammal005@gmail.com>
2026-03-15 10:40:45 +0100

    Add Gemelli

---
+++ 06-trees-and-bst/exercises/Gemelli/main.c
@@ -0,0 +1,37 @@
+#include "tree.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+Node* TreeCreateFromVectorRec(const int* v, size_t v_size, int i) {
+    if (i >= (int)v_size) {
+        return NULL;
+    }
+
+    Node* l = TreeCreateFromVectorRec(v, v_size, i * 2 + 1);
+    Node* r = TreeCreateFromVectorRec(v, v_size, i * 2 + 2);
+
+    return TreeCreateRoot(&v[i], l, r);
+}
+
+Node* TreeCreateFromVector(const int* v, size_t v_size) {
+    return TreeCreateFromVectorRec(v, v_size, 0);
+}
+
+extern bool TreesAreTwins(const Node* t1, const Node* t2);
+
+int main(void) {
+    int v[] = { 6, 4, 8, 2, 5 };
+    size_t v_size = sizeof(v) / sizeof(int);
+    Node* tree = TreeCreateEmpty();
+
+    tree = TreeCreateFromVector(v, v_size);
+
+    TreeWriteStdoutPreOrder(tree);
+
+    bool ris = TreesAreTwins(tree, tree);
+    printf("\nRis: %d\n", ris);
+
+    TreeDelete(tree);
+
+    return EXIT_SUCCESS;
+}
