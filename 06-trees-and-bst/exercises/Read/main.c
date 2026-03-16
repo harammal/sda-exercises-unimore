dfa798f33e5a199976c5d9edaca825a0992be028
harammal <hassanrammal005@gmail.com>
2026-03-16 11:01:06 +0100

    Add Read

---
+++ 06-trees-and-bst/exercises/Read/main.c
@@ -0,0 +1,32 @@
+#include "tree.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+Node* TreeCreateFromVectorRec(const int* v, size_t v_size, int i) {
+    if (i >= (int)v_size) {
+        return NULL;
+    }
+
+    Node* l = TreeCreateFromVectorRec(v, v_size, i * 2 + 1);
+    Node* r = TreeCreateFromVectorRec(v, v_size, i * 2 + 2);
+
+    return TreeCreateRoot(&v[i], l, r);
+}
+
+Node* TreeCreateFromVector(const int* v, size_t v_size) {
+    return TreeCreateFromVectorRec(v, v_size, 0);
+}
+
+extern Node* TreeRead(const char* filename);
+
+int main(void) {
+
+    Node* tree = TreeCreateEmpty();
+
+    tree = TreeRead("albero_01.txt");
+    TreeWriteStdoutPreOrder(tree);
+
+    TreeDelete(tree);
+
+    return EXIT_SUCCESS;
+}
