06636da818452eb0f41c52053ccbab67bc19bf13
harammal <hassanrammal005@proton.me>
2026-03-18 13:20:02 +0100

    Add MoveDownRec

---
+++ 07-heaps/exercises/MoveDownRec/move_down.c
@@ -0,0 +1,21 @@
+#include "minheap.h"
+
+void HeapMinMoveDownRec(Heap* h, int i) {
+    int smallest = i;
+    int l = HeapLeft(i);
+    int r = HeapRight(i);
+
+    if ((l < (int)h->size) && ElemCompare(HeapGetNodeValue(h, l), HeapGetNodeValue(h, smallest)) < 0) {
+        smallest = l;
+    }
+
+    if ((r < (int)h->size) && ElemCompare(HeapGetNodeValue(h, r), HeapGetNodeValue(h, smallest)) < 0) {
+        smallest = r;
+    }
+
+    if (smallest != i) {
+        ElemSwap(HeapGetNodeValue(h, i), HeapGetNodeValue(h, smallest));
+
+        HeapMinMoveDownRec(h, smallest);
+    }
+}
