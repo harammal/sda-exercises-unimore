8caaa3e419b05d170a9631f901cf41dd2e34c9d5
harammal <hassanrammal005@proton.me>
2026-03-18 13:18:17 +0100

    Add HeapSort

---
+++ 07-heaps/exercises/HeapSort/main.c
@@ -0,0 +1,22 @@
+#include "minheap.h"
+#include <stdlib.h>
+
+extern void HeapMinHeapsort(Heap* h);
+
+int main(void) {
+
+    Heap h = { .data = malloc(5 * sizeof(ElemType)), .size = 5 };
+    h.data[0] = 1;
+    h.data[1] = 2;
+    h.data[2] = 3;
+    h.data[3] = 4;
+    h.data[4] = 5;
+    HeapWriteStdout(&h);
+
+    HeapMinHeapsort(&h);
+    HeapWriteStdout(&h);
+
+    free(h.data);
+
+    return 0;
+}
